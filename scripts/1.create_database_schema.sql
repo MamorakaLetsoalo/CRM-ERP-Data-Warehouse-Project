a3542ea8d8fe8751f65a2b4f80b13684b1e988e0
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-12 15:40:29 +0200

    add script purpose

--- scripts/1.create_database_schema.sql
+++ scripts/1.create_database_schema.sql
@@ -1,11 +1,20 @@
+
+/*script purpose:
+this script creates a new database named CRM_ERP_DataWarehouse,the script additionally sets up schemas within the data base namely bronze,silver and gold*/
+
 --Create database 'DataWarehouse'
 
 CREATE DATABASE CRM_ERP_DataWarehouse
+GO
 
 USE CRM_ERP_DataWarehouse
 
 --create schemas
 
 CREATE SCHEMA bronze
+
+GO
 CREATE SCHEMA silver
+
+GO
 CREATE SCHEMA gold
