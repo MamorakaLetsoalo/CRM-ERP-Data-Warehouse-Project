8cec111561d990d82bdc8ed31ead66af86117ea1
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-12 16:20:25 +0200

    create product info table

--- scripts/2.create_tables.sql
+++ scripts/2.create_tables.sql
@@ -11,3 +11,14 @@ cst_gndr NVARCHAR(50),
 cst_create_date DATE
 )
 
+--Create product info table in the bronze layer
+
+CREATE TABLE bronze.crm_prd_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost INT,
+prd_line NVARCHAR(50),
+prd_start DATE,
+prd_end_dt DATE
+)
