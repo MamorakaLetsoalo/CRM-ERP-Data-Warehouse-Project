6aa84dbe133805d01d36a904fb6933d6951202c7
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-12 16:36:00 +0200

    add script purpose

--- scripts/2.create_tables.sql
+++ scripts/2.create_tables.sql
@@ -1,3 +1,13 @@
+/*
+===============================================================================
+DDL Script: Create Bronze Tables
+===============================================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema
+	  Run this script to re-define the DDL structure of 'bronze' Tables
+===============================================================================
+*/
+
 USE [CRM_ERP_DataWarehouse]
 
 --Create customer info table in the bronze layer
@@ -13,25 +23,26 @@ cst_create_date DATE
 
 --Create product info table in the bronze layer
 
-CREATE TABLE bronze.crm_prd_info(
-prd_id INT,
-prd_key NVARCHAR(50),
-prd_nm NVARCHAR(50),
-prd_cost INT,
-prd_line NVARCHAR(50),
-prd_start DATE,
-prd_end_dt DATE
-)
-
+CREATE TABLE bronze.crm_prd_info (
+    prd_id       INT,
+    prd_key      NVARCHAR(50),
+    prd_nm       NVARCHAR(50),
+    prd_cost     INT,
+    prd_line     NVARCHAR(50),
+    prd_start_dt DATETIME,
+    prd_end_dt   DATETIME
+);
 --Create sales details table in the bronze layer from data source CRM
 
-CREATE TABLE bronze.crm_sales_details(
-sls_ord_num NVARCHAR(50),
-sls_prd_key NVARCHAR(50),
-sls_cust_id INT,
-sls_ship_dt DATE,
-sls_due_dt DATE,
-sls_sales int,
-sls_quantity INT,
-sls_price INT
-)
+CREATE TABLE bronze.crm_sales_details (
+    sls_ord_num  NVARCHAR(50),
+    sls_prd_key  NVARCHAR(50),
+    sls_cust_id  INT,
+    sls_order_dt INT,
+    sls_ship_dt  INT,
+    sls_due_dt   INT,
+    sls_sales    INT,
+    sls_quantity INT,
+    sls_price    INT
+);
+
