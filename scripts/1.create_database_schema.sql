243b0e621a3ead3e412fecb651d6077bdfb8119f
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-12 15:31:09 +0200

    create database and schemas

---
+++ scripts/1.create_database_schema.sql
@@ -0,0 +1,11 @@
+--Create database 'DataWarehouse'
+
+CREATE DATABASE CRM_ERP_DataWarehouse
+
+USE CRM_ERP_DataWarehouse
+
+--create schemas
+
+CREATE SCHEMA bronze
+CREATE SCHEMA silver
+CREATE SCHEMA gold
