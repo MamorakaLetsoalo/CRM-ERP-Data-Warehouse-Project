958be5e844f2ad36a56cc1e5e702fc47e981a6fd
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-13 08:26:55 +0200

    load sales details table

--- scripts/3.load_tables.sql
+++ scripts/3.load_tables.sql
@@ -21,3 +21,12 @@ WITH(
      TABLOCK
      );
   
+  --Load data into product info table
+TRUNCATE TABLE [bronze].[crm_sales_details]
+BULK INSERT [bronze].[crm_sales_details]
+FROM 'C:\Users\Admin\Desktop\Data Engeneering Projects\CRM ERP Data warehouse project\CRM-ERP-Data-Warehouse-Project\raw_data\source CRM\sales_details.csv'
+WITH(
+     FIRSTROW = 2,
+     FIELDTERMINATOR = ',',
+     TABLOCK
+     );
