4a822043ba90c9f37462b67d6bce1a5551279daf
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-13 21:21:07 +0200

    rank the duplictes

--- scripts/silver layer/2.data_quality_and_cleaning.sql
+++ scripts/silver layer/2.data_quality_and_cleaning.sql
@@ -6,3 +6,10 @@ COUNT(*)
 FROM[bronze].[crm_cust_info]
 GROUP BY cst_id
 HAVING COUNT(*) > 1
+
+--window function to rank duplicates 
+
+SELECT *,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date desc) AS flag_last
+FROM [bronze].[crm_cust_info]
+WHERE cst_id = 29466
