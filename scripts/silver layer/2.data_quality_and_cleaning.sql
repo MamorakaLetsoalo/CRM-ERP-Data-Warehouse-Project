1a4d94f22ac2013e280caef8850220547feb33d1
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-13 21:29:13 +0200

    subquery to check all the data that is causing duplicates

--- scripts/silver layer/2.data_quality_and_cleaning.sql
+++ scripts/silver layer/2.data_quality_and_cleaning.sql
@@ -7,9 +7,17 @@ FROM[bronze].[crm_cust_info]
 GROUP BY cst_id
 HAVING COUNT(*) > 1
 
---window function to rank duplicates 
+--window function to rank duplicates to find the newest data
 
 SELECT *,
 ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date desc) AS flag_last
 FROM [bronze].[crm_cust_info]
 WHERE cst_id = 29466
+
+--subquery: Create a temporary table to see the data that is causing duplicates in the primary key
+SELECT *
+FROM(
+SELECT*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date desc) AS flag_last
+FROM [bronze].[crm_cust_info])t
+WHERE flag_last !=1 
