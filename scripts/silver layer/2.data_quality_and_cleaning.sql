8f4e8b10ef7764dc2c906d8ef74de76ccfa1bee3
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-14 20:51:15 +0200

    data standardization and consistency

--- scripts/silver layer/2.data_quality_and_cleaning.sql
+++ scripts/silver layer/2.data_quality_and_cleaning.sql
@@ -51,3 +51,25 @@ ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date desc) AS flag_la
 FROM [bronze].[crm_cust_info]
 WHERE cst_id IS NOT NULL)t
 WHERE flag_last =1
+
+--Data standardization and consistency 
+SELECT DISTINCT cst_gndr
+FROM bronze.crm_cust_info
+
+SELECT 
+cst_id,
+cst_key,
+TRIM(cst_firstname) AS cst_firstname,
+TRIM(cst_lastname) AS cst_lastname,
+cst_marital_status,
+CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female' -- Change the gender column to use full words insead of letter,trim any spaces,use upper for the code to run on any case
+	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+ELSE 'Unknown'
+END cst_gndr,
+cst_create_date
+FROM 
+( SELECT*,
+ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date desc) AS flag_last
+FROM [bronze].[crm_cust_info]
+WHERE cst_id IS NOT NULL)t
+WHERE flag_last =1
