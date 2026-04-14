8dce90dc2afa8ba12153a4357453b3a7c816004a
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-14 21:29:50 +0200

    create enrichment column to connect crm product table to erp

--- scripts/data quality and cleaning/3.clean_product_data.sql
+++ scripts/data quality and cleaning/3.clean_product_data.sql
@@ -6,3 +6,16 @@ COUNT(*)
 FROM[bronze].[crm_prd_info]
 GROUP BY prd_id
 HAVING COUNT(*) > 1
+
+
+--create cat-id column to connect to the erp product table
+SELECT
+    prd_id,      
+    prd_key,
+SUBSTRING (prd_key,1,5) AS cat_id,
+    prd_nm,      
+    prd_cost,     
+    prd_line,     
+    prd_start_dt, 
+    prd_end_dt
+FROM [bronze].[crm_prd_info]
