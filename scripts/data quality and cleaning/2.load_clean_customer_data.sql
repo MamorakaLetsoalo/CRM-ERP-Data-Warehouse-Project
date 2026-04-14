b25e98027eaae11d35d59907f0bd478cf6cbfb4c
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-14 21:15:38 +0200

    add comments to clean customer code data

--- scripts/data quality and cleaning/2.load_clean_customer_data.sql
+++ scripts/data quality and cleaning/2.load_clean_customer_data.sql
@@ -11,22 +11,24 @@ cst_create_date)
 SELECT 
 cst_id,
 cst_key,
+--data cleaning (trim unwanted spaces)
 TRIM(cst_firstname) AS cst_firstname,
 TRIM(cst_lastname) AS cst_lastname,
+--Data normalisation and standardization(maps coded values to meaningful,user-friendly descriptions)
 CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female' -- Change the gender column to use full words insead of letter,trim any spaces,use upper for the code to run on any case
 	 WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
 ELSE 'Unknown'
 END cst_gndr,
 CASE WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single' -- Change the marital status column to use full words insead of letter,trim any spaces,use upper for the code to run on any case
 	 WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
-ELSE 'Unknown'
+ELSE 'Unknown' --Handling missing data
 END cst_marital_status,
 cst_create_date
 FROM 
 ( SELECT*,
 ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date desc) AS flag_last
 FROM [bronze].[crm_cust_info]
-WHERE cst_id IS NOT NULL)t
-WHERE flag_last =1
+WHERE cst_id IS NOT NULL)t--remove duplicates
+WHERE flag_last =1 -- select the most recent record per customer
 
 SELECT * FROM [silver].[crm_cust_info]; 
