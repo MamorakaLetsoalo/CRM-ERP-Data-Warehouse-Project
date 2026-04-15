e7ff0218cad53f174785ab1a014c24d522124eca
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-15 14:39:57 +0200

    change date data type from INT to date in sales details

---
+++ scripts/data quality and cleaning/5.clean_sales_detail.sql
@@ -0,0 +1,15 @@
+--Change the data type from INT to DATE
+USE [CRM_ERP_DataWarehouse]
+SELECT
+    sls_ord_num ,
+    sls_prd_key,
+    sls_cust_id,
+CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
+     ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+END AS sls_order_dt,
+    sls_ship_dt,
+    sls_due_dt,
+    sls_sales,
+    sls_quantity,
+    sls_price
+FROM [bronze].[crm_sales_details]
