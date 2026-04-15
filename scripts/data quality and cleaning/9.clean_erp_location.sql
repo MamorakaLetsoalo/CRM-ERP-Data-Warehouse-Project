c4859d894c4386cc67f2b45dae7c07dde0ac5fda
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-15 16:49:58 +0200

    data standardisation and cinsistency for erp location

--- scripts/data quality and cleaning/9.clean_erp_location.sql
+++ scripts/data quality and cleaning/9.clean_erp_location.sql
@@ -3,3 +3,13 @@ SELECT
 REPLACE(cid, '-','')cid,
 cntry
 FROM [bronze].[erp_loc_a101]
+
+--datastandardisation and consistency 
+SELECT 
+REPLACE(cid, '-','')cid,
+CASE WHEN TRIM(cntry) = 'DE' THEN 'GERMANY'
+     WHEN TRIM(cntry) IN ('US' ,'USA') THEN 'UnitedS tates'
+     WHEN TRIM(cntry) = '' OR cntry IS NULL THEN 'Unknown'
+     ELSE TRIM(cntry)
+END AS cntry
+FROM [bronze].[erp_loc_a101]
