30e7d0b466bc94ef74342d5f48089a95d0b7bcd0
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-15 14:23:09 +0200

    load product info silver table with clean data

---
+++ scripts/data quality and cleaning/4.load_clean_product_data.sql
@@ -0,0 +1,30 @@
+--Load the customer info table with clean data
+
+INSERT INTO [silver].[crm_prd_info] (
+    prd_id,       
+    cat_id,     
+    prd_key,     
+    prd_nm,     
+    prd_cost,    
+    prd_line,    
+    prd_start_dt, 
+    prd_end_dt
+ )
+--cleaned data for silver layer
+SELECT
+    prd_id,
+--derived new columns based on transformations
+REPLACE(SUBSTRING (prd_key,1,5), '-','_') AS cat_id, --match data structure between the two tables(extract category)
+SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key, --specify the length of the column, product key used to join table sales details(extract product key)
+ prd_nm,
+ISNULL( prd_cost,0) AS prd_cost, --replace null values with zero
+CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain' -- Change the product line column to use full words insead of letter,trim any spaces,use upper for the code to run on any case
+	 WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+     WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+     WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Touring'
+     ELSE 'Unknown'
+END AS prd_line,
+CAST(prd_start_dt AS DATE) AS prd_start_dt, --cast the date column to change from datetime to date, remove the time stamps because they have no valuable information
+--Data enrichment
+CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_date
+FROM [bronze].[crm_prd_info]
