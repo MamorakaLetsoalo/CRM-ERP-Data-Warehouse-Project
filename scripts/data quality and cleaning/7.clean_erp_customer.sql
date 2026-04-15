92bd4ba5ce3920357a84738e476d8e6ee951fae6
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-15 16:09:07 +0200

    check the out of range dates

--- scripts/data quality and cleaning/7.clean_erp_customer.sql
+++ scripts/data quality and cleaning/7.clean_erp_customer.sql
@@ -7,3 +7,9 @@ END cid,
 bdate,
 gen
 FROM [bronze].[erp_cust_az12]
+
+--check the out of range dates
+SELECT DISTINCT
+bdate
+FROM [bronze].[erp_cust_az12]
+WHERE BDATE< '1924-01-01' OR bdate > GETDATE() --Check for cutomers olader than 100 years and customers with future value dates 
