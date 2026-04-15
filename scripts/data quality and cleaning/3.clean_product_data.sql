acef6535a6cb5c606554abac28f68b799bc5ae83
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-15 07:30:22 +0200

    standardise the product line column

--- scripts/data quality and cleaning/3.clean_product_data.sql
+++ scripts/data quality and cleaning/3.clean_product_data.sql
@@ -29,7 +29,25 @@ REPLACE(SUBSTRING (prd_key,1,5), '-','_') AS cat_id, --match data structure betw
 SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key, --specify the length of the column, product key used to join table sales details
  prd_nm,
  ISNULL( prd_cost,0) AS prd_cost, --replace null values with zero
-    prd_line,     
+    prd_line,
+    prd_start_dt, 
+    prd_end_dt
+FROM [bronze].[crm_prd_info]
+
+----Data standardization and consistency
+SELECT
+    prd_id,      
+    prd_key,
+REPLACE(SUBSTRING (prd_key,1,5), '-','_') AS cat_id, --match data structure between the two tables
+SUBSTRING(prd_key,7,LEN(prd_key)) AS prd_key, --specify the length of the column, product key used to join table sales details
+ prd_nm,
+ ISNULL( prd_cost,0) AS prd_cost, --replace null values with zero
+CASE WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain' -- Change the product line column to use full words insead of letter,trim any spaces,use upper for the code to run on any case
+	 WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+     WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+     WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Touring'
+     ELSE 'Unknown'
+END AS prd_line,
     prd_start_dt, 
     prd_end_dt
 FROM [bronze].[crm_prd_info]
