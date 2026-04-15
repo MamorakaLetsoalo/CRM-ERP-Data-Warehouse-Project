9590914e7181a86158128dcd3e326769e68de5bb
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-15 21:31:03 +0200

    data intergration for the three tables

--- scripts/gold layer/1.join_tables_dim_customer.sql
+++ scripts/gold layer/1.join_tables_dim_customer.sql
@@ -18,7 +18,24 @@ ON ci.cst_key = ca.cid
 LEFT JOIN [silver].[erp_loc_a101] la
 ON ci.cst_key = la.cid
 
-SELECT * FROM [silver].[erp_cust_az12];
+--Data intergration between the table to solve the mismatch
+SELECT
+ci.cst_id, 
+ci.cst_key,
+ci.cst_firstname,
+ci.cst_lastname,
+ci.cst_gndr,
+CASE WHEN ci.cst_marital_status != 'Unknown' THEN ci.cst_marital_status
+     ELSE COALESCE(ca.gen, 'Unknown')
+     END AS gen,
+ci.cst_create_date,
+ca.bdate,
+la.cntry
+FROM [silver].[crm_cust_info] ci
+LEFT JOIN [silver].[erp_cust_az12] ca
+ON ci.cst_key = ca.cid
+LEFT JOIN [silver].[erp_loc_a101] la
+ON ci.cst_key = la.cid
 
 
 
