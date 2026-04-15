0035504524dba3a7bd93ffacfae0e5065692c49b
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-15 16:15:02 +0200

    return NULL for birthdates in the future

--- scripts/data quality and cleaning/7.clean_erp_customer.sql
+++ scripts/data quality and cleaning/7.clean_erp_customer.sql
@@ -12,4 +12,16 @@ FROM [bronze].[erp_cust_az12]
 SELECT DISTINCT
 bdate
 FROM [bronze].[erp_cust_az12]
-WHERE BDATE< '1924-01-01' OR bdate > GETDATE() --Check for cutomers olader than 100 years and customers with future value dates 
+WHERE BDATE< '1924-01-01' OR bdate > GETDATE() --Check for cutomers olader than 100 years and customers with future value dates
+
+--Transformation
+SELECT
+CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4 ,LEN(cid))
+     ELSE cid
+END cid,
+CASE WHEN bdate > GETDATE() THEN NULL --if birthday is larger that current date then NULL
+     ELSE bdate
+     END AS bdate,
+gen
+FROM [bronze].[erp_cust_az12]
+
