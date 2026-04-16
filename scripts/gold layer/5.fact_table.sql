450ad610b8cb28c483a1def6d17adf3f9273ab7f
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-16 08:19:19 +0200

    create facts table

---
+++ scripts/gold layer/5.fact_table.sql
@@ -0,0 +1,16 @@
+--Create facts table
+SELECT
+sd.sls_ord_num,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt,
+sd.sls_ship_dt,
+sd.sls_due_dt,
+sd.sls_sales,
+sd.sls_quantity,
+sd.sls_price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id
