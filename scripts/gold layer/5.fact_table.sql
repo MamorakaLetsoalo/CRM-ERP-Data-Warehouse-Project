3ba1a6b237deeca12830dcea3c8cc49215bcb9b7
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-17 07:30:17 +0200

    data catalog

--- scripts/gold layer/5.fact_table.sql
+++ scripts/gold layer/5.fact_table.sql
@@ -1,14 +1,15 @@
 --Create facts table
+CREATE VIEW gold.fact_sales AS
 SELECT
-sd.sls_ord_num,
-pr.product_key,
-cu.customer_key,
-sd.sls_order_dt,
-sd.sls_ship_dt,
-sd.sls_due_dt,
-sd.sls_sales,
-sd.sls_quantity,
-sd.sls_price
+sd.sls_ord_num AS order_number,
+pr.product_key, --dimension key
+cu.customer_key, --dimension key
+sd.sls_order_dt AS order_date,
+sd.sls_ship_dt AS shiping_date,
+sd.sls_due_dt AS due_date,
+sd.sls_sales AS sales_amount,
+sd.sls_quantity AS quantity,
+sd.sls_price AS price
 FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
 ON sd.sls_prd_key = pr.product_number
